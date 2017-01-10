729e08d12ef0ebfb992e28d003c844045d87c68c
Michael Chen <michael.chen@capitalone.com>
2017-01-10 14:58:39 -0500

    Initial scaffolding

--- c7n/resources/dynamodb.py
+++ c7n/resources/dynamodb.py
@@ -16,12 +16,17 @@ import itertools
 from c7n.query import QueryResourceManager
 from c7n.manager import resources
 from c7n.utils import chunks, local_session
+from c7n.actions import ActionRegistry, BaseAction
+
+actions = ActionRegistry('dynamodb-table.actions')
 
 
 @resources.register('dynamodb-table')
 class Table(QueryResourceManager):
 
     resource_type = 'aws.dynamodb.table'
+
+    action_registry = actions
 
     def augment(self, resources):
 
@@ -33,3 +38,30 @@ class Table(QueryResourceManager):
         with self.executor_factory(max_workers=3) as w:
             return list(itertools.chain(
                 *w.map(_augment, chunks(resources, 20))))
+
+
+@actions.register('delete')
+class Delete(BaseAction):
+    """Deletes selected Dynamodb tables
+
+    This will delete Dynamodb tables. It is recommended to apply with a filter
+    to avoid deleting all Dynamodb tables in the account.
+    :example:
+        .. code-block: yaml
+            policies:
+              - name: dynamodb-delete
+                resource: dynamodb-table
+                filters:
+                  - default-vpc
+                actions:
+                  - type: delete
+    """
+    schema = {
+        'type': 'object',
+        'properties': {
+            'type': {'enum': ['delete']
+            }
+        }
+
+    def process(self, tables):
+        client = local_session(self.manager.session_factory).client('dynamodb-table')
